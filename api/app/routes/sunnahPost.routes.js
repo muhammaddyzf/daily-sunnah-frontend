35fe70245440a741dcf5ccfbe88eb6f9c2e7a4fa
muhammad yusuf <muhammadd.yzf@gmail.com>
2020-04-15 08:11:36 +0700

    remove backend resources

--- api/app/routes/sunnahPost.routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = (app) => {
-  const sunnahPosts = require('../controllers/sunnahPost.controller');
-
-  // Create Operation
-  app.post('/sunnah-posts', sunnahPosts.create);
-}
--- api/config/database.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  DB: 'mongodb://192.168.99.100:8383/db_daily_sunnah'
-}
